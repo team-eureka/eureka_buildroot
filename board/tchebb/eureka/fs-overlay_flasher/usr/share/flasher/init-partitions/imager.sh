00ff6779fd3c9d85835c8c219596ee44eeb08bf8
Thomas Hebb <tommyhebb@gmail.com>
2013-09-20 23:48:37 -0400

    flashcast: auto-expand partitions when init_partitions file is present

---
+++ board/tchebb/eureka/fs-overlay_flasher/usr/share/flasher/init-partitions/imager.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# I know this is horribly ugly. Believe me, it's the only way.
+fdisk -u '/dev/sda' <<EOF
+c
+d
+1
+n
+p
+1
+8
+
+
+t
+1
+c
+w
+EOF
+
+mkfs.fat -n 'FLASHCAST' '/dev/sda1'
