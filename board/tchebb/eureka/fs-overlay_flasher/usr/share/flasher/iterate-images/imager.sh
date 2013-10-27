5da711d3d91eca9ddc3514bebb89762514468a5c
Thomas Hebb <tommyhebb@gmail.com>
2013-10-26 23:32:50 -0400

    flashcast: don't set the IGNORE_ERRORS environment variable for mods

--- board/tchebb/eureka/fs-overlay_flasher/usr/share/flasher/iterate-images/imager.sh
+++ board/tchebb/eureka/fs-overlay_flasher/usr/share/flasher/iterate-images/imager.sh
@@ -2,6 +2,8 @@ if ! test "$#" -ge 1 -a -d "$1" ; then
 	fatal "Invalid argument"
 fi
 
+IGNORE_ERRORS="$2"
+
 for IMAGE_PATH in "$1"/* ; do
 	if ! flash-image "$IMAGE_PATH" ; then
 		if test -n "$IGNORE_ERRORS" ; then
