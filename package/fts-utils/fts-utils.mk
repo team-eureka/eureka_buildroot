96775c4eda12b1ea3a04bf8d61e1f37d14a712c1
Thomas Hebb <tommyhebb@gmail.com>
2013-10-27 07:04:59 -0400

    add fts-utils package

---
+++ package/fts-utils/fts-utils.mk
@@ -0,0 +1,21 @@
+################################################################################
+#
+# fts-utils
+#
+################################################################################
+
+FTS_UTILS_VERSION = v0.1
+FTS_UTILS_SITE = https://github.com/tchebb/fts-utils.git
+FTS_UTILS_SITE_METHOD = git
+
+define FTS_UTILS_BUILD_CMDS
+	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) fts-get fts-set
+endef
+
+define FTS_UTILS_INSTALL_TARGET_CMDS
+	$(INSTALL) -D -m 0755 $(@D)/fts-set $(TARGET_DIR)/usr/sbin
+	$(INSTALL) -D -m 0755 $(@D)/fts-get $(TARGET_DIR)/usr/sbin
+	$(INSTALL) -D -m 0755 $(@D)/set-boot-cmd $(TARGET_DIR)/usr/sbin
+endef
+
+$(eval $(generic-package))
