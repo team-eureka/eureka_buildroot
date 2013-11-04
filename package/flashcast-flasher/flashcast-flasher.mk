b130d21d1dfee04f7eaa25ee590e6d8a519cd3ab
Thomas Hebb <tommyhebb@gmail.com>
2013-11-04 15:40:40 -0500

    package/flashcast-flasher: build utils before install

--- package/flashcast-flasher/flashcast-flasher.mk
+++ package/flashcast-flasher/flashcast-flasher.mk
@@ -8,6 +8,10 @@ FLASHCAST_FLASHER_VERSION = master
 FLASHCAST_FLASHER_SITE = https://github.com/tchebb/flashcast-flasher.git
 FLASHCAST_FLASHER_SITE_METHOD = git
 
+define FLASHCAST_FLASHER_BUILD_CMDS
+	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) all
+endef
+
 define FLASHCAST_FLASHER_INSTALL_TARGET_CMDS
 	$(MAKE) DESTDIR="$(TARGET_DIR)" -C $(@D) install
 endef
