198bebe93542e6e6810b3aac373746a05bf894b1
Thomas Hebb <tommyhebb@gmail.com>
2014-02-02 23:42:07 -0500

    package/mesa3d-etna_viv: update from upstream

--- package/mesa3d-etna_viv/mesa3d-etna_viv.mk
+++ package/mesa3d-etna_viv/mesa3d-etna_viv.mk
@@ -9,7 +9,7 @@ MESA3D_ETNA_VIV_SITE = git://github.com/laanwj/mesa.git
 MESA3D_ETNA_VIV_AUTORECONF = YES
 MESA3D_ETNA_VIV_INSTALL_STAGING = YES
 
-MESA3D_ETNA_VIV_CONF_ENV = \
+MESA3D_ETNA_VIV_CONF_ENV := \
 	AWK="$(HOST_DIR)/usr/bin/gawk" \
 	LEX="$(HOST_DIR)/usr/bin/flex" \
 	YACC="$(HOST_DIR)/usr/bin/bison -y" \
@@ -18,16 +18,36 @@ MESA3D_ETNA_VIV_CONF_ENV = \
 	ETNA_LIBS="$(STAGING_DIR)/usr/lib/libetnaviv.a"
 
 MESA3D_ETNA_VIV_CONF_OPT = \
-	--enable-gles1 \
-	--enable-gles2 \
 	--disable-glx \
 	--enable-dri --with-dri-drivers= \
-	--enable-egl --with-egl-platforms=fbdev \
-	--enable-gallium-egl --with-gallium-drivers=swrast,etna \
-	--disable-static
+	--with-gallium-drivers=swrast,etna
+
+ifeq ($(BR2_PREFER_STATIC_LIB),y)
+MESA3D_ETNA_VIV_CONF_OPT += --enable-static --disable-shared
+else
+MESA3D_ETNA_VIV_CONF_OPT += --disable-static --enable-shared
+endif
 
 ifeq ($(BR2_PACKAGE_MESA3D_ETNA_VIV_DEBUG),y)
 MESA3D_ETNA_VIV_CONF_OPT += --enable-debug
+endif
+
+ifeq ($(BR2_PACKAGE_MESA3D_ETNA_VIV_EGL),y)
+MESA3D_ETNA_VIV_CONF_OPT += --enable-egl --with-egl-platforms=fbdev --enable-gallium-egl
+else
+MESA3D_ETNA_VIV_CONF_OPT += --disable-egl
+endif
+
+ifeq ($(BR2_PACKAGE_MESA3D_ETNA_VIV_GLES),y)
+MESA3D_ETNA_VIV_CONF_OPT += --enable-gles1 --enable-gles2
+else
+MESA3D_ETNA_VIV_CONF_OPT += --disable-gles1 --disable-gles2
+endif
+
+ifeq ($(BR2_PACKAGE_MESA3D_ETNA_VIV_VG),y)
+MESA3D_ETNA_VIV_CONF_OPT += --enable-openvg
+else
+MESA3D_ETNA_VIV_CONF_OPT += --disable-openvg
 endif
 
 MESA3D_ETNA_VIV_DEPENDENCIES = \
