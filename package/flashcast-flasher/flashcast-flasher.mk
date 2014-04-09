1ead3c7b5d128870bbb03b8cc8de3226d0cd18a8
Thomas Hebb <tommyhebb@gmail.com>
2014-04-08 23:16:13 -0400

    package/flashcast-flasher: bump to v1.2

--- package/flashcast-flasher/flashcast-flasher.mk
+++ package/flashcast-flasher/flashcast-flasher.mk
@@ -4,9 +4,11 @@
 #
 ################################################################################
 
-FLASHCAST_FLASHER_VERSION = v1.1.1
+FLASHCAST_FLASHER_VERSION = v1.2
 FLASHCAST_FLASHER_SITE = https://github.com/team-eureka/flashcast-flasher.git
 FLASHCAST_FLASHER_SITE_METHOD = git
+FLASHCAST_FLASHER_LICENSE = GPLv3+
+FLASHCAST_FLASHER_LICENSE_FILES = LICENSE
 
 define FLASHCAST_FLASHER_BUILD_CMDS
 	$(MAKE) CC="$(TARGET_CC)" LD="$(TARGET_LD)" -C $(@D) all
